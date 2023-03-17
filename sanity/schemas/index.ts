242612589b204c8bcc72356919a1dcb324b4b481
Henriette <Henriettehestsveen@gmail.com>
2023-03-17 11:26:51 +0100

    satt opp React, spurt QROQ

--- sanity/schemas/index.ts
+++ sanity/schemas/index.ts
@@ -1,4 +1,5 @@
 import categories from './category'
+import produkter from './produkter'
 
 //Kommuniserer med Sanity her, vi må lage skjemaer.
-export const schemaTypes = [categories]
+export const schemaTypes = [categories, produkter]
---
+++ sanity/schemas/produkter.js
@@ -0,0 +1,39 @@
+export default {
+    name: 'produkter',
+    title: 'Produktene',
+    type: 'document',
+
+    fields: [
+        {
+            name: 'Produkt_tittel',
+            title: 'Produktnavn',
+            type: 'string'
+        }, 
+
+        {
+            name: 'price',
+            type: 'number',
+            title: 'Pris' 
+
+        },
+
+        {
+            name: 'quantity',
+            type: 'number',
+            title: 'lagerstatus'
+        },
+
+        {
+            name: 'produkt_image',
+            type: 'image',
+            title: 'Prod_bilde'
+        },
+
+        {
+            name: 'category',
+            title: 'Kategori',
+            type: 'reference',
+            to: [{type: 'categories'}]
+        },
+    ]
+}
